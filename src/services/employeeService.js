240ad4321f77f411513ff24f85701d0e62c03a57
Joshua Smith <mtgmirage@protonmail.com>
2024-11-11 13:46:49 -0600

    added getAllEmployees() function to employeeService.js

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,3 +1,7 @@
 export const getEmployeeByEmail = (email) => {
     return fetch(`http://localhost:8088/employees?email=${email}`).then((res) => res.json())
 }
+
+export const getAllEmployees = () => {
+    return fetch(`http://localhost:8088/employees`).then((res) => res.json())
+}
