7de0fb95f0dce6fcc136ef804f949c7bef3f3bcd
Joshua Smith <mtgmirage@protonmail.com>
2024-11-11 13:50:17 -0600

    added basic employee state to EmployeeList.jsx

--- src/components/employees/EmployeeList.jsx
+++ src/components/employees/EmployeeList.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import { getAllEmployees } from "../../services/employeeService"
 
 
 export const EmployeeList = () => {
+    const [employees, setEmployees] = useState([])
+
+    const getAndSetEmployees = () => {
+        getAllEmployees().then((employeeArray) => {
+            setEmployees(employeeArray)
+        })
+    }
+
+    useEffect(() => {
+        getAndSetEmployees()
+    }, [])
 
     return (
         <div className="employees-list-panel">
