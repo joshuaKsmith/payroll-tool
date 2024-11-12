3a1bfecb8d6898a632ed586ea3ea9ac130928297
Joshua Smith <mtgmirage@protonmail.com>
2024-11-12 10:59:23 -0600

    basic layout for Shifts.css

--- src/components/shifts/Shifts.jsx
+++ src/components/shifts/Shifts.jsx
@@ -1,6 +1,29 @@
-
+import { useState } from "react"
+import "./Shifts.css"
 
 export const Shifts = ({ currentUser }) => {
+    const [date, setDate] = useState(new Date().toISOString().slice(0,10))
 
-    return (<>Shifts View</>)
+    const handleDateChange = (event) => {
+        setDate(event.target.value)
+    }
+
+    return (
+        <div className="shifts-view">
+            <div className="shifts-left-panel">
+                <input 
+                    className="shifts-date"
+                    type="date"
+                    value={date}
+                    onChange={handleDateChange}
+                />
+                <div className="shifts-new-entry">
+                    New Entry
+                </div>
+            </div>
+            <div className="shifts-right-panel">
+                Shifts
+            </div>
+        </div>
+    )
 }
