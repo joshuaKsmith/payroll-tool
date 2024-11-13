0f2b2fa58e7230aef12fb910580440552632a36b
Joshua Smith <mtgmirage@protonmail.com>
2024-11-13 10:19:31 -0600

    split seperate <NewShift /> component

---
+++ src/components/shifts/NewShift.jsx
@@ -0,0 +1,42 @@
+import { postNewShift } from "../../services/shiftService"
+
+
+export const NewShift = ({ handleEmployeeSelect, employees, handleHoursChange, getAndSetShifts, newShift }) => {
+
+    const handleShiftSubmission = (event) => {
+        event.preventDefault()
+        postNewShift(newShift).then(() => {
+            getAndSetShifts()
+        })
+    }
+
+    return (
+        <form className="shifts-new-entry" onSubmit={handleShiftSubmission}> 
+            <h2>New Entry</h2>
+            <select className="shifts-employee-select" id="shifts-select" onChange={handleEmployeeSelect}>
+                {employees.map((employee) => 
+                    <option
+                        id={employee.id}
+                        key={employee.id}
+                        value={employee.id}
+                    >
+                        {employee.fullName}
+                    </option>
+                )}
+            </select>
+            <label className="shifts-hours-label">
+                Hours Worked
+                <input 
+                    type="text"
+                    className="shifts-hours-input"
+                    id="shifts-hours-input"
+                    onChange={handleHoursChange}
+                    required
+                />
+            </label>
+            <button className="shifts-new-btn" type="submit">
+                Submit
+            </button>
+        </form>
+    )
+}
--- src/components/shifts/Shifts.jsx
+++ src/components/shifts/Shifts.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react"
 import "./Shifts.css"
 import { getAllEmployees } from "../../services/employeeService"
-import { getShiftsByDate, postNewShift } from "../../services/shiftService"
+import { getShiftsByDate } from "../../services/shiftService"
 import { Shift } from "./Shift"
+import { NewShift } from "./NewShift"
 
 export const Shifts = ({ currentUser }) => {
     const [date, setDate] = useState(new Date().toISOString().slice(0,10))
     const [employees, setEmployees] = useState([])
     const [newShift, setNewShift] = useState({
         date: new Date().toISOString().slice(0,10),
-        employeeId: 1,
+        employeeId: "1",
         length: "0"
     })
     const [shifts, setShifts] = useState([])
@@ -38,12 +39,6 @@ export const Shifts = ({ currentUser }) => {
         })
     }
 
-    const handleShiftSubmission = () => {
-        postNewShift(newShift).then(() => {
-            getAndSetShifts()
-        })
-    }
-
     useEffect(() => {
         getAllEmployees().then((employeeArray) => {
             setEmployees(employeeArray)
@@ -52,6 +47,10 @@ export const Shifts = ({ currentUser }) => {
 
     useEffect(() => {
         getAndSetShifts()
+        setNewShift((prev) => {
+            prev.date = date
+            return prev
+        })
     }, [date, employees])
 
     return (
@@ -63,36 +62,13 @@ export const Shifts = ({ currentUser }) => {
                     value={date}
                     onChange={handleDateChange}
                 />
-                <div className="shifts-new-entry">
-                    <h2>New Entry</h2>
-                    <select className="shifts-employee-select" id="shifts-select" onChange={handleEmployeeSelect}>
-                        {employees.map((employee) => 
-                            <option
-                                id={employee.id}
-                                key={employee.id}
-                                value={employee.id}
-                            >
-                                {employee.fullName}
-                            </option>
-                        )}
-                    </select>
-                    <label className="shifts-hours-label">
-                        Hours Worked
-                        <input 
-                            type="text"
-                            className="shifts-hours-input"
-                            id="shifts-hours-input"
-                            onChange={handleHoursChange}
-                            required
-                        />
-                    </label>
-                    <button
-                        className="shifts-new-btn"
-                        onClick={handleShiftSubmission}
-                    >
-                        Submit
-                    </button>
-                </div>
+                <NewShift 
+                    handleEmployeeSelect={handleEmployeeSelect}
+                    employees={employees}
+                    handleHoursChange={handleHoursChange}
+                    getAndSetShifts={getAndSetShifts}
+                    newShift={newShift}
+                />
             </div>
             <div className="shifts-right-panel">
                 <h2>Shifts</h2>
