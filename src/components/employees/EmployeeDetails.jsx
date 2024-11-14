878912f73d845cfc0b4fa18c2a61f155d7eccfbb
Joshua Smith <mtgmirage@protonmail.com>
2024-11-13 20:10:37 -0600

    styled employeeDetails view

--- src/components/employees/EmployeeDetails.jsx
+++ src/components/employees/EmployeeDetails.jsx
@@ -1,7 +1,68 @@
-
+import { useParams } from "react-router-dom"
+import "./EmployeeDetails.css"
 
 export const EmployeeDetails = () => {
 
 
-    return (<>Employee Details</>)
+    const { employeeId } = useParams()
+
+    return (
+        <div className="employee-profile">
+            <h1>Employee Profile</h1>
+            <form className="employee-edit">
+                <div className="employee-edit-form">
+                    <label>
+                        Name:
+                        <input 
+                        
+                        />
+                    </label>
+                    <label>
+                        Phone:
+                        <input 
+                        
+                        />
+                    </label>
+                    <label>
+                        Hire Date:
+                        <input 
+                            readOnly
+                        />
+                    </label>
+                    <label>
+                        Email:
+                        <input 
+                        
+                        />
+                    </label>
+                    <div className="employee-edit-box">
+                        <label className="employee-edit-rate-label">
+                            Rate:
+                            <input 
+                                className="employee-edit-rate-input"
+                            />
+                        </label>
+                        <label>
+                            Admin?
+                            <input 
+                                type="checkbox"
+                            />
+                        </label>
+                    </div>
+                </div>
+                <div className="profile-btn-container">
+                    <button
+                        className="profile-btn-save profile-btn"
+                    >
+                        Update
+                    </button>
+                    <button
+                        className="profile-btn-delete profile-btn"
+                    >
+                        Terminate
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
 }
